3a0b8faae55859462e1e4e97e0dd1da2a8a1e90e
DonDoanDev <ddoan10@my.centennialcollege.ca>
2023-02-21 14:55:49 -0500

    updated db URI to work with MongoDB Atlas

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,5 @@
 module.exports = 
 { 
-    "URI": "mongodb://127.0.0.1:27017/book_store"
+    //"URI": "mongodb://127.0.0.1:27017/book_store"
+    "URI": "mongodb+srv://DonnyDoan:BoBocute2103@atlascluster.d1xjjdl.mongodb.net/book_store?retryWrites=true&w=majority"
 }
